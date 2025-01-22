a72f579a8a8310fc2bc289a2f86d0584861c9d87
Singh, Aditya <Aditya.Singh@cvent.com>
2025-01-22 18:33:57 +0530

    made correction in the config file and did some little bit corrections

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import config from './config/config'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  console.log(config.appwriteUrl)
 
   return (
     <>
